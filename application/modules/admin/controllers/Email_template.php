112bf69cb1e3e8a0ba91c527874275d8ce4858ea
bgmmcn <bgmmcn@gmail.com>
2025-05-12 22:00:14 +0800

    application/modules/admin/controllers/Email_template.php

--- application/modules/admin/controllers/Email_template.php
+++ application/modules/admin/controllers/Email_template.php
@@ -1,54 +1,54 @@
 <?php
 
-defined('BASEPATH') or exit('This page does not exist');
+defined('BASEPATH') or exit('禁止直接访问');
 
 use MX\MX_Controller;
 
 /**
- * Email_template Controller Class
- * @property email_template_model $email_template_model email_template_model Class
+ * 邮件模板控制器类
+ * @property email_template_model $email_template_model 邮件模板模型类
  */
 class Email_template extends MX_Controller
 {
     public function __construct()
     {
-        // Make sure to load the administrator library!
+        // 确保加载管理员库
         $this->load->library('administrator');
         $this->load->model('email_template_model');
         $this->load->helper('file');
 
         parent::__construct();
 
-        requirePermission("viewBackups");
+        requirePermission("查看备份");
     }
 
     public function index()
     {
-        // Change the title
-        $this->administrator->setTitle("Email templates");
+        // 设置页面标题
+        $this->administrator->setTitle("邮件模板管理");
 
         $templates = $this->email_template_model->getTemplates();
 
-        // Prepare my data
+        // 准备模板数据
         $data = array(
             'url' => $this->template->page_url,
             'templates' => $templates,
         );
 
-        // Load my view
+        // 加载视图模板
         $output = $this->template->loadPage("email_template/index.tpl", $data);
 
-        // Put my view in the main box with a headline
-        $content = $this->administrator->box('Email templates', $output);
+        // 将视图放入带标题的主框中
+        $content = $this->administrator->box('邮件模板列表', $output);
 
-        // Output my content. The method accepts the same arguments as template->view
+        // 输出内容
         $this->administrator->view($content, false, "modules/admin/js/email_template.js");
     }
 
     public function edit($id)
     {
-        // Change the title
-        $this->administrator->setTitle("Edit template");
+        // 设置页面标题
+        $this->administrator->setTitle("编辑模板");
 
         if (!$id || !is_numeric($id)) {
             header('Location: ' . pageURL . 'admin/email_templates');
@@ -56,23 +56,22 @@ class Email_template extends MX_Controller
         }
 
         $template = $this->email_template_model->getTemplate($id);
-
         $content = read_file(APPPATH . '/views/email_templates/' . $template['template_name'] . '');
 
-        // Prepare my data
+        // 准备编辑数据
         $data = array(
             'url' => $this->template->page_url,
             'content' => $content,
             'template' => $template
         );
 
-        // Load my view
+        // 加载编辑视图
         $output = $this->template->loadPage("email_template/edit.tpl", $data);
 
-        // Put my view in the main box with a headline
-        $content = $this->administrator->box('Edit template', $output);
+        // 将视图放入带标题的主框中
+        $content = $this->administrator->box('编辑邮件模板', $output);
 
-        // Output my content. The method accepts the same arguments as template->view
+        // 输出内容
         $this->administrator->view($content, false, "modules/admin/js/email_template.js");
     }
 
@@ -82,11 +81,11 @@ class Email_template extends MX_Controller
         $template = $this->email_template_model->getTemplate($id);
 
         if (empty($content)) {
-            die("Content can't be empty!");
+            die("模板内容不能为空！");
         }
 
         if (!write_file(APPPATH . '/views/email_templates/' . $template['template_name'] . '', $content)) {
-            die('Unable to write the file');
+            die('文件写入失败');
         } else {
             die('yes');
         }
