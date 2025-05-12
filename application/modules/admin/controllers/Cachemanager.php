5d0719c598b723f1dee9aaba32d3d60781b87747
bgmmcn <bgmmcn@gmail.com>
2025-05-12 21:54:12 +0800

    application/modules/admin/controllers/Cachemanager.php

--- application/modules/admin/controllers/Cachemanager.php
+++ application/modules/admin/controllers/Cachemanager.php
@@ -3,8 +3,8 @@
 use MX\MX_Controller;
 
 /**
- * Cache Management Controller Class
- * @property cache_model $cache_model Cache Model Class
+ * 缓存管理控制器类
+ * @property cache_model $cache_model 缓存模型类
  */
 class Cachemanager extends MX_Controller
 {
@@ -14,7 +14,7 @@ class Cachemanager extends MX_Controller
 
     public function __construct()
     {
-        // Make sure to load the administrator library!
+        // 确保加载管理员库！
         $this->load->library('administrator');
 
         $this->itemMatches = ["spells/*", "items/*", "search/*"];
@@ -27,30 +27,30 @@ class Cachemanager extends MX_Controller
     }
 
     /**
-     * Display Cache Management Interface
+     * 显示缓存管理界面
      */
     public function index()
     {
-        // Set page title
-        $this->administrator->setTitle("Cache Management");
+        // 设置页面标题
+        $this->administrator->setTitle("缓存管理");
 
-        // Prepare my data
+        // 准备数据
         $data = array(
             'url' => $this->template->page_url
         );
 
-        // Load my view template
+        // 加载视图模板
         $output = $this->template->loadPage("cachemanager/cache.tpl", $data);
 
-        // Output to admin panel
-        $content = $this->administrator->box('Cache Management', $output);
+        // 输出到管理面板
+        $content = $this->administrator->box('缓存管理', $output);
 
-        // Output my content
+        // 输出内容
         $this->administrator->view($content, false, "modules/admin/js/cache.js");
     }
 
     /**
-     * Get Cache Information
+     * 获取缓存信息
      */
     public function get()
     {
@@ -61,7 +61,7 @@ class Cachemanager extends MX_Controller
         $total['files'] = $item['files'] + $website['files'] + $theme['files'];
         $total['size'] = $this->formatSize($item['size'] + $website['size'] + $theme['size']);
 
-        // Prepare my data
+        // 准备数据
         $data = [
             'url' => $this->template->page_url,
             'item' => $item,
@@ -70,73 +70,73 @@ class Cachemanager extends MX_Controller
             'total' => $total
         ];
 
-        // Load my view template
+        // 加载视图模板
         $output = $this->template->loadPage("cachemanager/cache_data.tpl", $data);
 
         die($output);
     }
 
     /**
-     * Count Item Cache
+     * 计算物品缓存
      */
     private function countItemCache(): array
     {
-        // Define my result
+        // 定义结果
         $result = [
             "files" => 0,
             "size" => 0
         ];
 
-        // Define what to search for
+        // 定义搜索条件
         $matches = $this->itemMatches;
 
-        // Loop through all searches
+        // 循环搜索
         return $this->SearchCache($matches, $result);
     }
 
     /**
-     * Count Theme Cache
+     * 计算主题缓存
      */
     private function countThemeMinifyCache(): array
     {
-        // Define my result
+        // 定义结果
         $result = [
             "files" => 0,
             "size" => 0
         ];
 
-        // Define what to search for
+        // 定义搜索条件
         $matches = $this->minifyMatches;
 
-        // Loop through all searches
+        // 循环搜索
         return $this->SearchCache($matches, $result);
     }
 
     /**
-     * Count Website Cache
+     * 计算网站缓存
      */
     private function countWebsiteCache(): array
     {
-        // Define my result
+        // 定义结果
         $result = [
             "files" => 0,
             "size" => 0
         ];
 
-        // Define what to search for
+        // 定义搜索条件
         $matches = $this->websiteMatches;
 
-        // Loop through all searches
+        // 循环搜索
         return $this->SearchCache($matches, $result);
     }
 
     /**
-     * Format Byte Size
+     * 格式化字节大小
      */
     private function formatSize($size): string
     {
         if ($size < 1024) {
-            return $size . " B";
+            return $size . " 字节";
         } elseif ($size < 1024 * 1024) {
             return round($size / 1024) . " KB";
         } elseif ($size < 1024 * 1024 * 1024) {
@@ -147,7 +147,7 @@ class Cachemanager extends MX_Controller
     }
 
     /**
-     * Delete Specific Type of Cache
+     * 删除特定类型缓存
      */
     public function delete($type = false)
     {
@@ -188,24 +188,24 @@ class Cachemanager extends MX_Controller
                     break;
             }
 
-            die("success");
+            die("删除成功");
         }
     }
 
     /**
-     * Search Cache
+     * 搜索缓存
      */
     private function SearchCache(array $matches, array $result): array
     {
         foreach ($matches as $search) {
-            // Search for matches
+            // 搜索匹配
             $matches = glob("writable/cache/data/" . $search);
 
             if ($matches) {
-                // Loop through all matches
+                // 循环匹配
                 foreach ($matches as $file) {
                     if (!preg_match("/index\.html/", $file)) {
-                        // Count and add size to result
+                        // 计数和添加大小到结果
                         $result['files']++;
                         $result['size'] += filesize($file);
                     }
