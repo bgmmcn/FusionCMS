2c4554735edb55b252d2de96afd2f99654e8fcd9
bgmmcn <bgmmcn@gmail.com>
2025-05-12 19:16:20 +0800

    application/config/Database.php

--- application/config/Database.php
+++ application/config/Database.php
@@ -4,68 +4,47 @@ namespace App\Config;
 
 use CodeIgniter\Database\Config;
 
-/**
- * Database Configuration
- */
 class Database extends Config
 {
-    /**
-     * The directory that holds the Migrations and Seeds directories.
-     */
-    public string $filesPath = APPPATH . 'Database' . DIRECTORY_SEPARATOR;
+    public string $defaultGroup = "cms";
 
-    /**
-     * Lets you choose which connection group to use if no other is specified.
-     */
-    public string $defaultGroup = 'cms';
-
-    /**
-     * The cms database connection.
-     *
-     * @var array<string, mixed>
-     */
     public array $cms = [
-        'DSN'          => '',
-        'hostname'     => 'localhost',
-        'username'     => '',
-        'password'     => '',
-        'database'     => '',
-        'DBDriver'     => 'MySQLi',
-        'DBPrefix'     => '',
-        'pConnect'     => false,
-        'DBDebug'      => true,
-        'charset'      => 'utf8mb4',
-        'DBCollat'     => 'utf8mb4_general_ci',
-        'swapPre'      => '',
-        'encrypt'      => false,
-        'compress'     => false,
-        'strictOn'     => false,
-        'failover'     => [],
-        'port'         => 3306
+        "DSN" => "",
+        "hostname" => "127.0.0.1",
+        "username" => "root",
+        "password" => "root",
+        "database" => "1_cms",
+        "DBDriver" => "MySQLi",
+        "DBPrefix" => "",
+        "pConnect" => false,
+        "DBDebug" => true,
+        "charset" => "utf8mb4",
+        "DBCollat" => "utf8mb4_general_ci",
+        "swapPre" => "",
+        "encrypt" => false,
+        "compress" => false,
+        "strictOn" => false,
+        "failover" => [],
+        "port" => 3306,
     ];
 
-    /**
-     * The account database connection.
-     *
-     * @var array<string, mixed>
-     */
     public array $account = [
-        'DSN'          => '',
-        'hostname'     => 'localhost',
-        'username'     => '',
-        'password'     => '',
-        'database'     => '',
-        'DBDriver'     => 'MySQLi',
-        'DBPrefix'     => '',
-        'pConnect'     => false,
-        'DBDebug'      => true,
-        'charset'      => 'utf8mb4',
-        'DBCollat'     => 'utf8mb4_general_ci',
-        'swapPre'      => '',
-        'encrypt'      => false,
-        'compress'     => false,
-        'strictOn'     => false,
-        'failover'     => [],
-        'port'         => 3306
+        "DSN" => "",
+        "hostname" => "127.0.0.1",
+        "username" => "root",
+        "password" => "root",
+        "database" => "1_auth",
+        "DBDriver" => "MySQLi",
+        "DBPrefix" => "",
+        "pConnect" => false,
+        "DBDebug" => false,
+        "charset" => "utf8",
+        "DBCollat" => "utf8_general_ci",
+        "swapPre" => "",
+        "encrypt" => false,
+        "compress" => false,
+        "strictOn" => false,
+        "failover" => [],
+        "port" => 3306,
     ];
 }
