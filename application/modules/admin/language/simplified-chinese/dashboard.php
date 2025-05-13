e96b584c10f61215bca322ed61d100946378dbf6
bgmmcn <bgmmcn@gmail.com>
2025-05-13 15:42:02 +0800

    application/modules/admin/language/simplified-chinese/dashboard.php

---
+++ application/modules/admin/language/simplified-chinese/dashboard.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang["important"] = "重要";
+$lang["new"] = "新内容";
+$lang["update_available"] = "发现新版本更新";
+$lang["update_download"] = "您可以通过%s更新到最新版本";
+$lang["here"] = "此处";
+$lang["theme_old"] = "您的主题已过时";
+$lang["theme_old_detail"] = "部分页面可能无法正常显示，请联系前端开发人员更新主题模板";
