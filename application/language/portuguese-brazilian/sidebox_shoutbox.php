38592855463a27dbd430d5fc78e9ce9ea77fc5e3
bgmmcn <bgmmcn@gmail.com>
2025-05-14 07:07:24 +0800

    Delete application/language/*/

--- application/language/portuguese-brazilian/sidebox_shoutbox.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$lang['ago'] = "atrás";
-$lang['view_profile'] = "Ver perfil";
-$lang['said'] = "disse";
-$lang['message_limit'] = "A mensagem deve ter entre 0-255 caracteres!";
-$lang['enter'] = "Digite uma mensagem...";
-$lang['submit'] = "Enviar mensagem";
-$lang['log_in'] = "Por favor faça login para enviar";
--- application/language/spanish/sidebox_shoutbox.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$lang['ago'] = "hace";
-$lang['view_profile'] = "Ver perfil";
-$lang['said'] = "dicho";
-$lang['message_limit'] = "¡El mensaje debe tener entre 0 y 255 carácteres de largo!";
-$lang['enter'] = "Inserta un mensaje...";
-$lang['submit'] = "Envia el mensaje";
-$lang['log_in'] = "Por favor, inicia sesión para enviar el mensaje";
